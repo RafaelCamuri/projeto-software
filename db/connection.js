72242dcef09f40e23fa7ba3453eb528dad3cf4b9
RafaelCamuri <rafael.camuri@aluno.faculdadeimpacta.com.br>
2024-10-06 18:26:22 -0300

    Add files via upload

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Rafa!123',
+  database: 'projeto_software'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  } else {
+    console.log('Conectado ao banco de dados MySQL.');
+  }
+});
+
+module.exports = connection;
